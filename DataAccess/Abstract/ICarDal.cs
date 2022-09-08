f2f7e97edd177151780a2981de4f75e2856f3075
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2022-09-08 23:11:53 +0300

    I created car rental system

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+  public  interface ICarDal
+    {
+        List<Car> GetAll();
+
+        List<Car> GetById(int ID);
+        
+        void UpDate(Car car);
+
+        void Add(Car car);
+
+        void Delete(Car car);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+   public class InMemoryCarDal :ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                    new Car {Id=1,ColorId=1, BranId=1, DailyPrice=1500, Description="powerful engine", ModelYear=2010 },
+                    new Car {Id=2,ColorId=1, BranId=7, DailyPrice=2500, Description="powerful brake", ModelYear=2015 } ,
+                    new Car {Id=3,ColorId=2, BranId=5, DailyPrice=4500, Description="fuel saving and strong traction", ModelYear=2020 },
+                    new Car {Id=4,ColorId=3, BranId=1, DailyPrice=2500, Description="powerful engine", ModelYear=2015 },
+                    new Car {Id=5,ColorId=5, BranId=6, DailyPrice=1000, Description="eye-catching color", ModelYear=2009 },
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            //LINQ
+            Car carToDelete = _cars.SingleOrDefault(c => c.ModelYear < 2010);
+            _cars.Remove(carToDelete);
+
+        }
+
+        public void UpDate(Car car)
+        {
+            Car carUpDate = _cars.SingleOrDefault(c => c.ModelYear < 2010);
+            carUpDate.ModelYear = car.ModelYear;
+            carUpDate.Description = car.Description;
+            carUpDate.BranId = car.BranId;
+            carUpDate.ColorId = car.ColorId;
+            carUpDate.DailyPrice = car.DailyPrice;
+        }
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int ID)
+        {
+            return _cars.Where(c => c.Id == ID).ToList();
+        }
+    }
+}
